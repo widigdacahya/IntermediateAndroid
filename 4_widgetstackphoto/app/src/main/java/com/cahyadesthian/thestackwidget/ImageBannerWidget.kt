eee967d6429d7004cf8f0be4aae7b71af7bf5621
widigdacahya <rizkiwidigda@gmail.com>
2022-04-05 01:48:18 +0700

    adding photo for banner
    photo from unsplash and pexels <3

--- 4_widgetstackphoto/app/src/main/java/com/cahyadesthian/thestackwidget/ImageBannerWidget.kt
+++ 4_widgetstackphoto/app/src/main/java/com/cahyadesthian/thestackwidget/ImageBannerWidget.kt
@@ -37,7 +37,7 @@ internal fun updateAppWidget(
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.image_banner_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    //views.setTextViewText(R, widgetText)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
