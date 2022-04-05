c9f57ee1383e3ec7961b1cf7b681fcbcffe9616d
widigdacahya <rizkiwidigda@gmail.com>
2022-04-05 14:36:59 +0700

    Running Web View
    implement WebVIew in xml, addin permission in manifest, in mainActivity set load url on the web view then magic happen, we also need to javascriptenabled because it default false, as we know that javascript in website is awesome so just enable it, and there is toast that overrided from onPageFinished

--- 5_webview/app/src/main/java/com/cahyadesthian/thewebview/MainActivity.kt
+++ 5_webview/app/src/main/java/com/cahyadesthian/thewebview/MainActivity.kt
@@ -2,10 +2,31 @@ package com.cahyadesthian.thewebview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView_mainUI)
+        supportActionBar?.hide()
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                Toast.makeText(this@MainActivity, "Your web already loaded \uD83C\uDF08 ", Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+        webView.loadUrl("https://cahyadesthian.com/")
+        //webView.loadUrl("https://www.dicoding.com")
     }
+
+
+
 }
