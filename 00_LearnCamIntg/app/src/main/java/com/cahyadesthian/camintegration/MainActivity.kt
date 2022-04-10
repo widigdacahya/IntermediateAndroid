7c4a5625e5c0b647959e8fed489547f24e039c53
widigdacahya <rizkiwidigda@gmail.com>
2022-04-10 18:45:48 +0700

    MainACtivity
    the logic to run cam integration, still show the taken picture in low quality, minTargetSdk 29 and there is a need of permission on read external storage to show the taken picutre

--- 00_LearnCamIntg/app/src/main/java/com/cahyadesthian/camintegration/MainActivity.kt
+++ 00_LearnCamIntg/app/src/main/java/com/cahyadesthian/camintegration/MainActivity.kt
@@ -1,14 +1,61 @@
 package com.cahyadesthian.camintegration
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import com.cahyadesthian.camintegration.databinding.ActivityMainBinding
+
+
+//what number
+private const val REQUEST_CODE = 13579
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mainBinding.root)
+
+
+        mainBinding.btnTakPhotoMainUI.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            if(takePictureIntent.resolveActivity(this.packageManager)!=null) {
+                startActivityForResult(takePictureIntent,REQUEST_CODE)
+            } else {
+                Toast.makeText(this,"No Cam", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
 
 
 
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val takenImage = data?.extras?.get("data")as Bitmap
+            mainBinding.imgViewMainUI.setImageBitmap(takenImage)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
+
+    }
+
+
+
+
+
 }
