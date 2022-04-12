918c67697bd02b0a7f3fff9107f0579041ba96aa
widigdacahya <rizkiwidigda@gmail.com>
2022-04-12 12:35:53 +0700

    video explayer done

--- 9_Media/Exoplayer/app/src/main/java/com/cahyadesthian/exovideoplayer/MainActivity.kt
+++ 9_Media/Exoplayer/app/src/main/java/com/cahyadesthian/exovideoplayer/MainActivity.kt
@@ -2,10 +2,35 @@ package com.cahyadesthian.exovideoplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cahyadesthian.exovideoplayer.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
+
+        val player = ExoPlayer.Builder(this).build()
+        viewBinding.exoplateVidplaterMainUI.player = player
+
+        val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
+        player.setMediaItem(mediaItem)
+        player.prepare()
+
+
+    }
+
+
+
+    companion object {
+        const val URL_VIDEO_DICODING = "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
     }
 }
