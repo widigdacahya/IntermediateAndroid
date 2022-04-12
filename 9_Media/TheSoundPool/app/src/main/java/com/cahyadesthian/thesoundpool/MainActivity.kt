dc0847b7134626aa46978ae72f5a5a378e15f8ab
widigdacahya <rizkiwidigda@gmail.com>
2022-04-12 09:38:02 +0700

    prepare sound
    sound less than 1mb, downloaded form freesound.org about bird

--- 9_Media/TheSoundPool/app/src/main/java/com/cahyadesthian/thesoundpool/MainActivity.kt
+++ 9_Media/TheSoundPool/app/src/main/java/com/cahyadesthian/thesoundpool/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.cahyadesthian.thesoundpool
 
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sp : SoundPool
+    private var soundId : Int = 0
+    private var spLoaded = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sp = SoundPool.Builder()
+            .setMaxStreams(10)
+            .build()
+
+
+        sp.setOnLoadCompleteListener { _, _, status ->
+
+            if(status == 0) spLoaded = true else Toast.makeText(this@MainActivity, "Load Failed", Toast.LENGTH_SHORT).show()
+
+        }
+
+        soundId = sp.load(this, R.raw.bird_gaje, 1)
+
     }
 }
