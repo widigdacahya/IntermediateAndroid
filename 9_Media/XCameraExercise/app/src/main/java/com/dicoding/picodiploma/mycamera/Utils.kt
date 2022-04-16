80f6e75129d2c2dafbea7bcca13425371171c593
widigdacahya <rizkiwidigda@gmail.com>
2022-04-16 08:42:24 +0700

    using tempFile function of util and some permission

--- 9_Media/XCameraExercise/app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
+++ 9_Media/XCameraExercise/app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
@@ -16,11 +16,13 @@ val timeStamp: String = SimpleDateFormat(
     Locale.US
 ).format(System.currentTimeMillis())
 
+//untuk intentCamera things
 fun createTempFile(context: Context): File {
     val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
     return File.createTempFile(timeStamp, ".jpg", storageDir)
 }
 
+//untuk cameraX
 fun createFile(application: Application): File {
     val mediaDir = application.externalMediaDirs.firstOrNull()?.let {
         File(it, application.resources.getString(R.string.app_name)).apply { mkdirs() }
