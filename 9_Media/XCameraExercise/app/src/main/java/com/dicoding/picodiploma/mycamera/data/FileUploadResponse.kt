ec16183fab2e5ec406a261fcc30fc12262b63731
widigdacahya <rizkiwidigda@gmail.com>
2022-04-16 09:37:33 +0700

    networking things

---
+++ 9_Media/XCameraExercise/app/src/main/java/com/dicoding/picodiploma/mycamera/data/FileUploadResponse.kt
@@ -0,0 +1,13 @@
+package com.dicoding.picodiploma.mycamera.data
+
+import com.google.gson.annotations.SerializedName
+
+data class FileUploadResponse(
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String
+
+
+)
---
+++ 9_Media/XCameraExercise/app/src/main/java/com/dicoding/picodiploma/mycamera/networking/ApiConfig.kt
@@ -0,0 +1,24 @@
+package com.dicoding.picodiploma.mycamera.networking
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+
+    fun getApiService(): ApiService {
+        val logginInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logginInterceptor)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://story-api.dicoding.dev/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+
+}
