e9fdb7ce8153e33b50b23ccce0333f70f4713bfb
widigdacahya <rizkiwidigda@gmail.com>
2022-04-18 21:28:22 +0700

    show hamburger menu
    cant click  yet, but there is change on layout main, previewous we use fragmentcontainer view, it doenst has navControler, after change to fragent only, it has

--- 00_GetRidNavigation/app/src/main/java/com/cahyadesthian/ridnav/MainActivity.kt
+++ 00_GetRidNavigation/app/src/main/java/com/cahyadesthian/ridnav/MainActivity.kt
@@ -2,10 +2,33 @@ package com.cahyadesthian.ridnav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.cahyadesthian.ridnav.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController : NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        navController  = findNavController(R.id.fragmentContainerView)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        mainBinding.navigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
     }
 }
