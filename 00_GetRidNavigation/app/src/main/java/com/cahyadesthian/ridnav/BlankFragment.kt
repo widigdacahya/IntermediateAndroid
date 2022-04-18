891f13a6f02c22b10b3b52920376bc1962010bf3
widigdacahya <rizkiwidigda@gmail.com>
2022-04-18 18:25:38 +0700

    View Binding Fragment

--- 00_GetRidNavigation/app/src/main/java/com/cahyadesthian/ridnav/BlankFragment.kt
+++ 00_GetRidNavigation/app/src/main/java/com/cahyadesthian/ridnav/BlankFragment.kt
@@ -5,19 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cahyadesthian.ridnav.databinding.FragmentBlankBinding
 
 
 class BlankFragment : Fragment() {
 
-
+    private var _blankFragBinding : FragmentBlankBinding? = null
+    private val blanFragBinding get() = _blankFragBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        //val view = inflater.inflate(R.layout.fragment_blank, container, false)
+        _blankFragBinding = FragmentBlankBinding.inflate(inflater,container,false)
+        val view = blanFragBinding.root
+        return view
     }
 
-    
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _blankFragBinding = null
+    }
+
+
+
 }
