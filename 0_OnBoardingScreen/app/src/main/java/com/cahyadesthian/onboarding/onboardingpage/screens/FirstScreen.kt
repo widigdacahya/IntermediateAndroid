e535e16f53e5a06e1309c1dabd1f7ed9e64c7333
widigdacahya <rizkiwidigda@gmail.com>
2022-04-19 10:31:02 +0700

    next form first and second fragemtn
    changing third layout next tet to finish and implemented click listeenr on textview of fist and second fragemtn

--- 0_OnBoardingScreen/app/src/main/java/com/cahyadesthian/onboarding/onboardingpage/screens/FirstScreen.kt
+++ 0_OnBoardingScreen/app/src/main/java/com/cahyadesthian/onboarding/onboardingpage/screens/FirstScreen.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.cahyadesthian.onboarding.R
 import com.cahyadesthian.onboarding.databinding.FragmentFirstScreenBinding
 
@@ -23,6 +24,12 @@ class FirstScreen : Fragment() {
 
         _firstScreenFragBinding = FragmentFirstScreenBinding.inflate(inflater,container,false)
         val view = firstScreenFragBinding.root
+
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.viewpager)
+
+        firstScreenFragBinding.tvFirstNext.setOnClickListener {
+            viewPager?.currentItem = 1
+        }
 
         return view
 
--- 0_OnBoardingScreen/app/src/main/java/com/cahyadesthian/onboarding/onboardingpage/screens/SecondScreen.kt
+++ 0_OnBoardingScreen/app/src/main/java/com/cahyadesthian/onboarding/onboardingpage/screens/SecondScreen.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.cahyadesthian.onboarding.R
 import com.cahyadesthian.onboarding.databinding.FragmentSecondScreenBinding
 
@@ -22,6 +23,12 @@ class SecondScreen : Fragment() {
 
         _secondScreenFragBinding = FragmentSecondScreenBinding.inflate(inflater,container,false)
         val view = secondScreenFragBinding.root
+
+        val viewPager2 = activity?.findViewById<ViewPager2>(R.id.viewpager)
+
+        secondScreenFragBinding.tvSecondNext.setOnClickListener {
+            viewPager2?.currentItem = 2
+        }
 
         return view
 
