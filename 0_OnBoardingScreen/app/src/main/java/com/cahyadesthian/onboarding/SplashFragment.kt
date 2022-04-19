72cb303b1754b271cf604461b39e4a33d139c98e
widigdacahya <rizkiwidigda@gmail.com>
2022-04-19 10:09:53 +0700

    view pager done
    slashing change fragment

--- 0_OnBoardingScreen/app/src/main/java/com/cahyadesthian/onboarding/SplashFragment.kt
+++ 0_OnBoardingScreen/app/src/main/java/com/cahyadesthian/onboarding/SplashFragment.kt
@@ -1,10 +1,12 @@
 package com.cahyadesthian.onboarding
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.cahyadesthian.onboarding.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
@@ -21,6 +23,13 @@ class SplashFragment : Fragment() {
 
         _splashFragBinding = FragmentSplashBinding.inflate(inflater,container,false)
         val view = splasFragBinding.root
+
+
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
+        },1000)
+
+
 
         return view
 
