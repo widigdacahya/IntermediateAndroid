cb61a0f490ef33293bb509dddf9ce80f4036024b
widigdacahya <rizkiwidigda@gmail.com>
2022-04-25 17:17:10 +0700

    main view model
    there is state flow

--- 0_StateFlow/app/src/main/java/com/cahyadesthian/thestateflow/MainActivity.kt
+++ 0_StateFlow/app/src/main/java/com/cahyadesthian/thestateflow/MainActivity.kt
@@ -2,10 +2,27 @@ package com.cahyadesthian.thestateflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cahyadesthian.thestateflow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _mainBinding: ActivityMainBinding? = null
+    private val mainBinding get() = _mainBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
     }
+
+
+    override fun onDestroy() {
+
+        super.onDestroy()
+        _mainBinding = null
+
+    }
+
 }
---
+++ 0_StateFlow/app/src/main/java/com/cahyadesthian/thestateflow/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.cahyadesthian.thestateflow
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    //state flow
+    private val _loginUiStete = MutableStateFlow<LoginUiState>(LoginUiState.Empty)
+    val loginUiState : StateFlow<LoginUiState> = _loginUiStete
+
+
+    //custom type to passed on stateflow type yang empty
+    sealed class LoginUiState {
+        object Success : LoginUiState()
+        data class Error(val message: String) : LoginUiState()
+        object Loading : LoginUiState()
+        object Empty: LoginUiState()
+    }
+
+
+    fun login(username: String, password: String) = viewModelScope.launch {
+        _loginUiStete.value = LoginUiState.Loading
+        delay(2000L)
+        if(username == "android" && password == "cahya") {
+            _loginUiStete.value = LoginUiState.Success
+        } else {
+            _loginUiStete.value = LoginUiState.Error("Wrong Password \uD83E\uDD0F")
+        }
+    }
+
+}
