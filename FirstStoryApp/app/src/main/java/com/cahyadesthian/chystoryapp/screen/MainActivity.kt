3e126f9f2340f3336cf7bc79859325fa04b92f75
widigdacahya <rizkiwidigda@gmail.com>
2022-05-22 21:34:38 +0700

    login create
    showing login success, next add stroy list

--- FirstStoryApp/app/src/main/java/com/cahyadesthian/chystoryapp/screen/MainActivity.kt
+++ FirstStoryApp/app/src/main/java/com/cahyadesthian/chystoryapp/screen/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.cahyadesthian.chystoryapp.screen
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.cahyadesthian.chystoryapp.R
+
+
+internal val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "session")
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
