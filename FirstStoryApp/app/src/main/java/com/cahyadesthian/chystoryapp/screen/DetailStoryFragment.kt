9fb906c7f09397935331a9a2f29b1bcc7ad524d9
widigdacahya <rizkiwidigda@gmail.com>
2022-05-23 09:23:03 +0700

    detail story shown

--- FirstStoryApp/app/src/main/java/com/cahyadesthian/chystoryapp/screen/DetailStoryFragment.kt
+++ FirstStoryApp/app/src/main/java/com/cahyadesthian/chystoryapp/screen/DetailStoryFragment.kt
@@ -6,17 +6,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.cahyadesthian.chystoryapp.R
+import com.cahyadesthian.chystoryapp.databinding.FragmentDetailStoryBinding
+import com.cahyadesthian.chystoryapp.model.ItemListStory
+import com.cahyadesthian.chystoryapp.screen.util.glideLoad
 
 
 class DetailStoryFragment : Fragment() {
+
+    private var _detailStoryFragBinding : FragmentDetailStoryBinding? = null
+    private val detailStoryBinding get() = _detailStoryFragBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_story, container, false)
+
+        _detailStoryFragBinding = FragmentDetailStoryBinding.inflate(inflater,container,false)
+        return detailStoryBinding?.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val storyData = DetailStoryFragmentArgs.fromBundle(arguments as Bundle).story
+        setStory(storyData)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _detailStoryFragBinding = null
+    }
+
+    private fun setStory(storyItem : ItemListStory) {
+        detailStoryBinding?.apply {
+            ivStoryDetail.glideLoad(storyItem.photoUrl)
+            tvStoryUsername.text = storyItem.name
+            tvStoryDescription.text = storyItem.description
+        }
     }
 
 }
