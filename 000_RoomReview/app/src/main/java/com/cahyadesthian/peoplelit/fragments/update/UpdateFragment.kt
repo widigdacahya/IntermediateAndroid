105a30f67a23f5d901e6c8f1a9a7b70636ea2cdc
widigdacahya <rizkiwidigda@gmail.com>
2022-06-25 16:18:28 +0700

    current item data implemented

--- 000_RoomReview/app/src/main/java/com/cahyadesthian/peoplelit/fragments/update/UpdateFragment.kt
+++ 000_RoomReview/app/src/main/java/com/cahyadesthian/peoplelit/fragments/update/UpdateFragment.kt
@@ -5,11 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.navArgs
 import com.cahyadesthian.peoplelit.R
 
 
 class UpdateFragment : Fragment() {
 
+    private val args by navArgs<UpdateFragmentArgs>()
+
+    private lateinit var edtFirstName: EditText
+    private lateinit var edtLastName: EditText
+    private lateinit var edtAge: EditText
+    private lateinit var btnUpdate: Button
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        edtFirstName = view.findViewById(R.id.edtfirstname_update)
+        edtFirstName.setText(args.currentPeople.firstName)
+
+        edtLastName = view.findViewById(R.id.edtlastname_update)
+        edtLastName.setText(args.currentPeople.lastName)
+
+        edtAge = view.findViewById(R.id.edtage_update)
+        edtAge.setText(args.currentPeople.age.toString())
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
