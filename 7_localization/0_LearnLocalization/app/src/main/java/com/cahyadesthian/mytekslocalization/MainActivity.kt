0f06e0d7397d554da2df47e15843c424d652b934
widigdacahya <rizkiwidigda@gmail.com>
2022-09-28 16:26:03 +0700

    setting icon

--- 7_localization/0_LearnLocalization/app/src/main/java/com/cahyadesthian/mytekslocalization/MainActivity.kt
+++ 7_localization/0_LearnLocalization/app/src/main/java/com/cahyadesthian/mytekslocalization/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.cahyadesthian.mytekslocalization
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import android.widget.MediaController
 import com.cahyadesthian.mytekslocalization.databinding.ActivityMainBinding
 
@@ -19,12 +22,22 @@ class MainActivity : AppCompatActivity() {
         mediaController.setAnchorView(mainActivityBinding.videoview)
         mainActivityBinding.videoview.setMediaController(mediaController)
         supportActionBar?.hide()
+
+        setupView()
     }
 
 
 
     private fun setupView() {
-
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
 
 
     }
