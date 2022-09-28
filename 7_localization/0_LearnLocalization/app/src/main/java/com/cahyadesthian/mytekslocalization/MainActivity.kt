d1645bc486e772e7b99057e165fb348c625dfea8
widigdacahya <rizkiwidigda@gmail.com>
2022-09-28 16:18:29 +0700

    nice setting for video view with framelayout

--- 7_localization/0_LearnLocalization/app/src/main/java/com/cahyadesthian/mytekslocalization/MainActivity.kt
+++ 7_localization/0_LearnLocalization/app/src/main/java/com/cahyadesthian/mytekslocalization/MainActivity.kt
@@ -2,6 +2,7 @@ package com.cahyadesthian.mytekslocalization
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
 import com.cahyadesthian.mytekslocalization.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainActivityBinding.root)
+
+        mainActivityBinding.videoview.setVideoPath("android.resource://"+ packageName +"/"+R.raw.wonderland)
+        val mediaController =  MediaController(this)
+        mediaController.setAnchorView(mainActivityBinding.videoview)
+        mainActivityBinding.videoview.setMediaController(mediaController)
+        supportActionBar?.hide()
     }
+
+
+
+    private fun setupView() {
+
+
+
+    }
+
+    private fun setuplang() {
+
+    }
+
+
 }
