5f49039802940170e58e0f2c3ebc884cefecaa0e
widigdacahya <rizkiwidigda@gmail.com>
2022-09-28 15:38:57 +0700

    add learn of localization

---
+++ 7_localization/0_LearnLocalization/app/src/main/java/com/cahyadesthian/mytekslocalization/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.cahyadesthian.mytekslocalization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cahyadesthian.mytekslocalization.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainActivityBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivityBinding.root)
+    }
+}
